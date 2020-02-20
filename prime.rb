bb2bbfdbde44d8b3c17510f5fb6dc952f86d6a8f
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-20 00:17:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,5 @@
 # Add  code here!
+<<<<<<< HEAD
 def prime?(n)
   if n <= 1
         false
@@ -7,4 +8,12 @@ def prime?(n)
     else
         (2..n/2).none? { |i| n % i == 0}
     end
+=======
+def prime?(num)
+  if num % 2
+    true
+  elsif num % 2 > -1
+    false
+  end
+>>>>>>> 559fcad7b09d66e521c5c805f349cbc28fe052e0
 end
