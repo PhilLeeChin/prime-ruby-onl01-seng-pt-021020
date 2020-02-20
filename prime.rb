3f3371682da19d8fd4778892924541251384611f
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-20 00:18:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,8 @@
 # Add  code here!
-<<<<<<< HEAD
 def prime?(n)
   if n <= 1
         false
     elsif n % 2
         true
     end
-=======
-def prime?(num)
-  if num % 2
-    true
-  elsif num % 2 > -1
-    false
-  end
->>>>>>> 559fcad7b09d66e521c5c805f349cbc28fe052e0
 end
